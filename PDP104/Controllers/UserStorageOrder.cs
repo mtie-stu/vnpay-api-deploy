d7532c87cd8772611520669087b2faa8be42eba3
Đỗ Minh Tiến <mtie290305@gmail.com>
2025-03-31 23:43:58 +0700

    Dot

---
+++ PDP104/Controllers/UserStorageOrder.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PDP104.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserStorageOrder : ControllerBase
+    {
+
+
+    }
+}
