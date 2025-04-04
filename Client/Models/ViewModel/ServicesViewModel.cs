4f1464c200c3a74f2e79d4372db5ecd06c51e0d8
Đỗ Minh Tiến <mtie290305@gmail.com>
2025-04-05 00:32:07 +0700

    Controller_Frontend

---
+++ Client/Models/ViewModel/ServicesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Client.Models;
+
+namespace PDP104.ViewModel
+{
+    public class ServicesViewModel
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Tên Dịch Vụ")]
+        public string NameServices { get; set; }
+
+        public TypeService TypeService { get; set; }
+        public StatusService StatusService { get; set; }
+
+        [Display(Name = "Giá tiền dịch vụ/ Ngày")]
+        public decimal UnitPrice { get; set; }
+
+    }
+}
