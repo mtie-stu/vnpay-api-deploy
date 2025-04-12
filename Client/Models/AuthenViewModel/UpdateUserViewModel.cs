6a4906486ddb02421af4ebb5ec229334bd04dcd2
Đỗ Minh Tiến <mtie290305@gmail.com>
2025-04-12 17:52:15 +0700

    Feature_UpdateUser_Admin

---
+++ Client/Models/AuthenViewModel/UpdateUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Client.Models.AuthenViewModel
+{
+    public class UpdateUserViewModel
+    {
+        public string NameND { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public IFormFile? ImageFile { get; set; } // Ảnh đại diện gửi lên API
+    }
+}
